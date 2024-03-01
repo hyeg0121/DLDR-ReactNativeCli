6aa90e3ea0e8a990fa9a1edec82715f8aadb4d63
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-01 13:53:53 +0900

    feat: 색상 상수 모음

---
+++ src/styles/colors.tsx
@@ -0,0 +1,28 @@
+export const colors = {
+  // primary
+  primary: '#1638D8',
+  lighten1: '#5C74E4',
+  lighten2: '#8D9EED',
+  lighten3: '#B0BCF3',
+  lighten4: '#D7DDF8',
+  lighten5: '#EAEDFB',
+
+  // secondary
+  secondary1: '#FFC812',
+  secondary2: '#FFE9A0',
+  // gray
+  gray1: '#E6E6E6',
+  gray2: '#D9D9D9',
+  gray3: '#CCCCCC',
+  gray4: '#666666',
+  gray5: '#666666',
+
+  // black
+  black: '#000000',
+
+  // white
+  white: '#FFFFFF',
+
+  // green
+  green: '#34C759',
+};
