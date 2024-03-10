2e3c3debd0f9489929992b3d9089449e43f06f4a
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-10 20:33:10 +0900

    feat: DissmissKeyboardView 타입 정의

--- src/components/util/DissmissKeyboardView.tsx
+++ src/components/util/DissmissKeyboardView.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import {TouchableWithoutFeedback, Keyboard} from 'react-native';
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scrollview';
 
-const DismissKeyboardView = ({children, ...props}) => (
+const DismissKeyboardView: React.FC<{style?: StyleProp<ViewStyle>}> = ({
+  children,
+  ...props
+}) => (
   <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
     <KeyboardAwareScrollView {...props} style={props.style}>
       {children}
