eff9e0c8913af5d34fcd8c99af3feb1e86eabb05
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-22 23:00:11 +0900

    feat: slice 필드 수정

--- src/slices/user.ts
+++ src/slices/user.ts
@@ -1,20 +1,23 @@
 import {createSlice} from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   id: '',
   name: '',
   email: '',
-  accessToken: '',
-  refreshToken: '',
+  profileImg: '',
+  userType: '',
 };
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     setUser(state, action) {
+      state.id = action.payload.id;
       state.email = action.payload.email;
       state.name = action.payload.name;
-      state.accessToken = action.payload.accessToken;
+      state.profileImg = action.payload.profileImg;
+      state.userType = action.payload.userType;
     },
   },
   extraReducers: builder => {},
