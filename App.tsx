8c604469e4acb59569fdf9faa2be58a5ab311393
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-22 22:16:37 +0900

    feat: RootStackParamList를 AppInner에 옮김

--- App.tsx
+++ App.tsx
@@ -2,13 +2,6 @@ import * as React from 'react';
 import store from './src/store';
 import {Provider} from 'react-redux';
 import AppInner from './AppInner.tsx';
-
-// 로그인이 안 된 경우
-export type RootStackParamList = {
-  SignIn: undefined;
-  SignUp: undefined;
-};
-
 function App() {
   return (
     <Provider store={store}>
